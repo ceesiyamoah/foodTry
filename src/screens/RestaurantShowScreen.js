d4422d0dee4ae759fbdbb653498c02e6eda3366f
ceesiyamoah <ccyamoah@gmail.com>
2020-12-09 14:55:56 +0000

    Implemented new screen for selected restaurant

--- src/screens/RestaurantShowScreen.js
+++ src/screens/RestaurantShowScreen.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { Text, Image, View, StyleSheet } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import zomato from '../apis/zomato';
+import { AntDesign } from '@expo/vector-icons';
 
 //!Finish implemnting the api
 
 const RestaurantShowScreen = ({ navigation }) => {
-	const [restaurant, setRestaurant] = useState({});
+	const [restaurant, setRestaurant] = useState(null);
 	const [error, setError] = useState(false);
 	const id = navigation.getParam('id');
 	useEffect(() => {
@@ -17,18 +18,39 @@ const RestaurantShowScreen = ({ navigation }) => {
 						res_id: id,
 					},
 				});
-				setRestaurant(data.R);
+				setRestaurant(data);
 			} catch (error) {
 				setError(true);
 			}
 		};
 		fetchRestaurant();
 	}, []);
+	if (!restaurant) {
+		return null;
+	}
+
 	return (
 		<>
-			<Text>id: {id}</Text>
+			<Image
+				source={{
+					uri:
+						restaurant.featured_image ||
+						'https://b.zmtcdn.com/data/pictures/8/16774318/b16e382e9f6696f911b600b7e5ca6839.jpg',
+				}}
+				style={styles.image}
+			/>
 		</>
 	);
 };
+
+const styles = StyleSheet.create({
+	image: {
+		width: 300,
+		margin: 10,
+		aspectRatio: 1,
+		borderRadius: 5,
+		alignSelf: 'center',
+	},
+});
 
 export default RestaurantShowScreen;
